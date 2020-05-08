0e5767c648732716b373ab681c73f7a594a9fe6d
liuye923 <liuye923@gmail.com>
2020-05-08 12:19:03 -0700

    Create axis_in_middle.py

---
+++ axis_in_middle.py
@@ -0,0 +1,9 @@
+# Move left y-axis and bottim x-axis to centre, passing through (0,0)
+ax.spines['left'].set_position('center')
+ax.spines['bottom'].set_position('center')
+# Eliminate upper and right axes
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+# Show ticks in the left and lower axes only
+ax.xaxis.set_ticks_position('bottom')
+ax.yaxis.set_ticks_position('left')
