79bc181deeee173e61b2548758feab3ecd953ac4
liuye923 <liuye923@gmail.com>
2020-06-12 15:56:30 -0700

    Update monthly_to_year_month.py

--- monthly_to_year_month.py
+++ monthly_to_year_month.py
@@ -1,1 +1,9 @@
 #reshape monthly data to year-month
+#dat is a xarray dataframe
+import pandas as pd
+
+yyyymm = pd.MultiIndex.from_arrays([dat.coords['time.year'], dat.coords['time.month']], name=('year', 'month'))
+dat.coords['yyyymm'] = ('time', yyyymm)
+dat = dat.swap_dims({'time': 'yyyymm'})
+
+dat_year_mont = dat.unstack()
